e57e929613971ae83e53b2dcec82da63dc11c672
jihye_son <jiiihyeee98@gmail.com>
2022-11-28 18:25:52 +0900

    버튼 생성

--- app/Input.js
+++ app/Input.js
@@ -1,35 +1,37 @@
-import React from "react";
-import {View, TextInput, StyleSheet} from 'react-native'
+import React from 'react';
+import {View, TextInput, StyleSheet} from 'react-native';
 
-const Input = ({inputValue, inputChange}) =>(   //inputValue와 inputChange prop을 비구조화
-    <View style ={styles.inputContainer}>
-        <TextInput
-            value ={inputValue}
-            style ={styles.input}
-            placeholder ='What needs to be done?'
-            placeholderTextColor='#CACACA'
-            selectionColor='#666666'
-            onChangeText ={inputChange} //onChangeText메소드를 inputChange로 지정
-        />
-    </View>
-)
+const Input = (
+  {inputValue, inputChange}, //inputValue와 inputChange prop을 비구조화
+) => (
+  <View style={styles.inputContainer}>
+    <TextInput
+      value={inputValue}
+      style={styles.input}
+      placeholder="What needs to be done?"
+      placeholderTextColor="#CACACA"
+      selectionColor="#666666"
+      onChangeText={inputChange} //onChangeText메소드를 inputChange로 지정
+    />
+  </View>
+);
 
 const styles = StyleSheet.create({
-    inputContainer:{
-        marginLeft:20,
-        marginRight:20,
-        shadowOpacity: 0.2,
-        shadowRadius:3,
-        shadowColor: '#000000',
-        shadowOffset: { width:2, height:2}
-    },
+  inputContainer: {
+    marginLeft: 20,
+    marginRight: 20,
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    shadowColor: '#000000',
+    shadowOffset: {width: 2, height: 2},
+  },
 
-    input:{
-        height: 60,
-        backgroundColor :'#ffffff',
-        paddingLeft : 10,
-        paddingRight :10
-    }
-})
+  input: {
+    height: 60,
+    backgroundColor: '#ffffff',
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+});
 
-export default Input
+export default Input;
